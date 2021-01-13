30f447110ee77a8c9286d9e48878380029e683c3
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-01-13 18:50:22 +0300

    Add Metas

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,7 +8,37 @@ function App({ Component, pageProps, router }) {
   return (
     <>
       <Head>
-        <title key="title">Swiper - {meta.metaTitle || meta.title}</title>
+        <title key="title">
+          {meta.metaTitle ||
+            meta.title ||
+            "Swiper - The Most Modern Mobile Touch Slider"}
+        </title>
+        <meta
+          property="og:title"
+          content="Swiper - The Most Modern Mobile Touch Slider"
+        />
+        <meta
+          property="og:description"
+          content="Swiper is the most modern free mobile touch slider with hardware accelerated transitions and amazing native behavior."
+        />
+        <meta
+          property="og:image"
+          content="https://swiperjs.com/images/share-banner.png"
+        />
+        <meta property="og:site_name" content="Swiper" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@nolimits4web" />
+        <meta name="twitter:creator" content="@nolimits4web" />
+        <meta name="twitter:title" content="Swiper" />
+        <meta
+          name="twitter:description"
+          content="The Most Modern Mobile Touch Slider"
+        />
+        <meta
+          name="twitter:image"
+          content="https://swiperjs.com/images/share-banner.png"
+        />
+        <link rel="shortcut icon" href="/images/favicon.png" />
       </Head>
       {router.pathname !== "/" && <Header />}
       <Component {...pageProps} />
