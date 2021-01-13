b32b3130c51cb5816459bd2bc58bf542775e2b77
Vlad <vl.tansky@gmail.com>
2021-01-13 22:31:22 +0200

    chore: typedoc json

--- build/build-types.js
+++ build/build-types.js
@@ -1,5 +1,8 @@
+const fs = require("fs-extra");
 const { promise: exec } = require("exec-sh");
-
+const dir = "./public/types";
+fs.removeSync(dir);
+fs.ensureDirSync(dir);
 exec(
-  "npx typedoc --out ./public/types ./node_modules/swiper --includeDeclarations --excludeExternals"
+  `npx typedoc --json ./src/types.json ./node_modules/swiper/types --includeDeclarations --excludeExternals`
 );
